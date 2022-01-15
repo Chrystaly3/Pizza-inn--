207ad750f430e85952f82c57350bb97c159917f0
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2022-01-15 19:44:59 +0300

    JQuery function

--- app.js
+++ app.js
@@ -6,3 +6,43 @@ order.prototype.fullOrder= function(){
 	return this.size + "Pizza crust of " + this.crust + "and topping of" + this.toppings;
 };
 };
+
+function total(quantity, pizzaPrice, crustPrice,toppingPrice,delivery){
+	this.quantity= quantity
+	this.pizzaPrice = pizzaPrice
+	this.crustPrice = crustPrice
+	this.toppingPrice = toppingPrice
+	this.delivery = delivery
+total.prototype.totalCash = function(){
+	return this.quantity * (this.pizzaPrice + this.crustPrice + this.toppingPrice + this.delivery);
+};
+};
+
+function validate(){
+	let location = document.getElementById("locate").value
+	let name = $("#myName").val();
+	let phone = $("#phone").val();
+
+
+	if(location != "" && phone != "" && phone.length === 10){
+		document.getElementById("show-saved").innerHTML = name + "order will be deliverd soon" + location + "!"
+		$(".showing").slideToggle();
+		$(".confirmThis").slideToggle();
+				$(document).on("click","#Submit-btn",function(e){
+				e.preventDefault();
+
+		
+					});
+		
+}else{
+
+	return false
+}	
+}
+$(document).ready(function(){
+	$('form#myform').submit(function(event){
+		event.preventDefault();
+		shipping
+
+	})
+})
