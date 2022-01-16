60cbc1417f8aa59353ed58370e716bd3095d8a10
Chrystaly3 <josephine.nzioka@student.moringaschool.com>
2022-01-16 18:52:39 +0300

    modify file

---
+++ README.md
@@ -0,0 +1,58 @@
+# IP4
+
+# Date:16/01/2022
+
+# By Josephine Mueni Nzioka
+
+# Description
+
+Delani Studio project
+
+## Design
+
+"From our past role our firm is responsible for creating cohesive and visually attractive front-end designs. We also do Back-end design to ensure the websites are responsive and user-friendly."
+
+## Development
+Our developers are competent and fluent with latest technologies.
+
+## Product Management
+
+Planning and development its impotant part in our firm since we build codes from scratch and test them to see their fuctionality and user friendly.
+
+# Technology used;
+
+## 1.HTML
+
+## 2.CSS
+
+## 3.JavaScript
+
+## 4. Bootstrap
+
+## 5. JQUERY
+
+
+<img src="asset/assets/Delani-Studio.png">
+
+
+<img src="asset/assets/Delani-Studio2.png">
+
+
+
+<img src="asset/assets/Delani-Studio3.png">
+
+# Support and contact details
+
+This code is open for any improvement.Incase you need my
+contacts find them below thanks.
+
+#### Email:josephine.nzioka@student.moringaschool.com
+
+#### Phone: 0712811416
+
+# License
+License permission is hereby granted ,free of charge to anyone obtaining a copy of these software and associated documentation to deal in the software without restrictions including without limitation the rights to use ,copy modify ,publish and distribute copies of these software and to permit persons to whom the software is furnished to do so,subject to the following conditions:
+
+The above copyright notice should be included.
+
+Copyright (c) 2021
--- app.js
+++ app.js
@@ -1,127 +1,236 @@
-function order(size,crust,toppings){
-	this.size = size
-	this.crust = crust
-	this.toppings = toppings
-order.prototype.fullOrder= function(){
-	return this.size + "Pizza crust of " + this.crust + "and topping of" + this.toppings;
+function Order( size, crust, topping) {
+	this.size = size;
+	this.crust = crust;
+	this.topping = topping;
+
+
+Order.prototype.fullOrder = function () {
+	return   this.size + ' pizza with a crust of ' + this.crust + " and a topping of " + this.topping;
+};
+};
+function Total(quantity, pizzaPrice,crustPrice,toppingPrice,shipped){
+	this.quantity = quantity;
+	this.pizzaPrice = pizzaPrice;
+	this.crustPrice = crustPrice;
+	this.toppingPrice = toppingPrice;
+	this.shipped = shipped;
+
+
+
+Total.prototype.totalCash = function() {
+	return  this.quantity * (this.pizzaPrice + this.crustPrice + this.toppingPrice + this.shipped )
 };
 };
 
-function total(quantity, pizzaPrice, crustPrice,toppingPrice,delivery){
-	this.quantity= quantity
-	this.pizzaPrice = pizzaPrice
-	this.crustPrice = crustPrice
-	this.toppingPrice = toppingPrice
-	this.delivery = delivery
-total.prototype.totalCash = function(){
-	return this.quantity * (this.pizzaPrice + this.crustPrice + this.toppingPrice + this.delivery);
-};
-};
 
-function validate(){
-	let location = document.getElementById("locate").value
-	let name = $("#myName").val();
-	let phone = $("#phone").val();
+	function validate() {
+ 
+			var myLocation = document.getElementById('locate').value;
+			var myName = $('#myName').val();
+			var myPhone = $('#phone').val();
+		
+
+	
+			if (myLocation != ''&& myPhone !=''&& myPhone.length ===10) {
+				 document.getElementById('show-saved').innerHTML = myName + " Lucky for you we are only 15 minutes away from " + myLocation +" !"
+				 $('.showing').slideToggle();
+				 $('.comfirmThis').slideToggle();
+						$(document).on('click', '#submit-btn', function(e){
+							 e.preventDefault();
+					 });
+
+				 
+		
+			}
+			else{ 
+					 return false;
+			
+					}
+				 
+	
+	}
+	
+
+//business logic
+$(document).ready(function () {
+	$("form#myform").submit(function(event) {
+			event.preventDefault();
+			myShipping = parseInt($('#show-locate').val());
+			var myName = $('#myName').val();
+	
+
+					var pizzaSize = $('#size').val();
+
+					function price(){
+
+							if(pizzaSize=='Small'){
+									return 600;
+							}else if(pizzaSize=='Medium'){
+									return 800;
+							}else if(pizzaSize=='Large'){
+									return 1200
+							}
+					
+					}
 
 
-	if(location != "" && phone != "" && phone.length === 10){
-		document.getElementById("show-saved").innerHTML = name + "order will be deliverd soon" + location + "!"
-		$(".showing").slideToggle();
-		$(".confirmThis").slideToggle();
-				$(document).on("click","#Submit-btn",function(e){
-				e.preventDefault();
+					var pizzaCrust = $('#crust').val();
+					 var myCrust = crustiePrice()
+
+					function crustiePrice(){
+							if(pizzaCrust==='Cracker'){
+									return 250;
+							}else if(pizzaCrust==='Stuffed'){
+									return 200;
+
+							}else if(pizzaCrust==='Cheese'){
+									return 250;
+
+						 
+							}
+					}
+							//Toppings
+					var pizzaTopping = checkedToppings();
+
+						 function checkedToppings() {
+							var topName = document.getElementsByName('mole');
+
+							var topLength = topName.length;
+							var topValue = ' ';
+							for(i = 0; i<topLength; i++){
+									var topChecked = topName[i].checked;
+									if(topChecked){
+											topValue += topName[i].value + ' ; ' ;
+									}   
+							}
+							var result = topValue;
+							return result;
+
+
+					}
+
+							//Quantity
+					var pizzaQuantity = parseInt($('#quantity').val());
+
+					var mySize = price()
+
+
+					var myTopping =checkBox();
+
+					 function checkBox(){
+							var chb = document.getElementsByName('mole');
+							var box1 = 150;
+							var box2 = 180;
+							var box3 = 150;
+							
+									 
+	if (chb[0].checked){
+											document.getElementById("errMessage").innerHTML = "";
+											return box1;
+
+							}else if(chb[1].checked){
+																	document.getElementById("errMessage").innerHTML = "";
+																	return box2; 
+																	 
+							}else if(chb[2].checked){
+																	document.getElementById("errMessage").innerHTML = "";
+																	return box3;
+
+							}else if(chb[3].checked){
+
+																	document.getElementById("errMessage").innerHTML = "";
+																	return box4;
+																	
+							}else if(chb[4].checked){
+																	document.getElementById("errMessage").innerHTML = "";
+																	return box5;
+																	
+							}else if(chb[5].checked){
+																	document.getElementById("errMessage").innerHTML = "";
+																	return box6;
+							}else{
+												 document.getElementById("errMessage").innerHTML = "choose atleast one topping!";
+
+									
+							}
+
+					}
+	//Derivery
+					var check = checkBox();
+
+					var myShipping = addDelivery();
+					function addDelivery(){
+							var checkDelivery = document.getElementsByName('weDeliver');
+							if(checkDelivery[0].checked){
+									var money =  200;
+									return money;
+							}else if(!checkDelivery[0].checked){
+									var noMoney = 0;
+									return noMoney;
+							}
+			 
+		
+					}
+					
+		
+			
+		 var  newTotal= new Total(pizzaQuantity,mySize,myCrust,myTopping,myShipping);
+
+		 if( myName!='' && pizzaSize !='' &&  pizzaCrust != ''  && pizzaQuantity !='' && check  ){
+			$('.hide').fadeIn();
+			$('.image-section').hide();
+			$('#pizzSize').text( pizzaSize);
+			$('#pizzCrust').text( pizzaCrust);
+			$('#pizzTopping').text( pizzaTopping);
+			$('#pizzQuantity').text( pizzaQuantity);
+			$('#pizzTotal').text( newTotal.totalCash());
+
+			$('#each-size').text( 'pizza-size : '+ pizzaSize);
+			$('#each-crust').text( 'Crust : ' + pizzaCrust);
+			$('#each-top').text( 'Toppings : ' + pizzaTopping);
+			$('#each-qty').text( 'Your order : ' + pizzaQuantity);
+			$('#each-total').text( 'Your total bill : ' + newTotal.totalCash() );
+			$('#each-text').text('Thank You for choosing us to serve you ' + myName + ' !'); 
+
+			
+		 
+
+	}else {
+
+			return false;
+
+
+
+	}
+ 
+
+
+	 
+	$('.addedMe').off().click(function(){
+			$('#newOrder').append(
+					'<tr><th id="pizzTable">'+ '#' + '</th><td id="pizzSize">' + pizzaSize + '</td><td id="pizzCrust">' + pizzaCrust +
+							 '</td><td id="pizzTopping">' + pizzaTopping + '</td><td id="pizzQuantity">' + pizzaQuantity + '</td><td id="pizzTotal">' + newTotal.totalCash() 
+		 );
+	});
+
+	if($('#myName') !=''){
+
+			$('.addedMe').fadeIn();
+			$('.addMe').hide();
+	};
+ 
+
+			});
 
 		
-					});
-		
-}else{
+	
 
-	return false
-}	
-}
-$(document).ready(function(){
-	$('form#myform').submit(function(event){
-		event.preventDefault();
-		shipping = parseInt($(#show-locate).val());
-		let name = $("#myName").val();
-		let pizzaSize = $("#size").val();
-		
-		function price(){
-			if(pizzaSize == "Small"){
-				return 600;
-			}else if(pizzaSize == "Medium"){
-				return 800;
-			}else if(pizzaSize == "Large"){
-				return 1200;
-			}
-		}
-		let pizzaCrust = $("#crust").val();
-		let crust = crustPrice()
-//Crust
-		function crustPrice(){
-			if(pizzaCrust === "cheese" ){
-				return 200
-			}else if(pizzaCrust === "thin"){
-				return 300
-			}else if(pizzaCrust === "stuffed"){
-				return 400
-			}else if(pizzaCrust === "thick"){
-				return 500
-			}
-		}
-		//Toppings
-		let pizzaToppings = checkedToppings();
-		function checkedToppings(){
-			let toppingName = document.getElementByName("mole");
-			let toppingLength = toppingName.length;
-			let toppingValue = " ";
-		for(i = 0; i < toppingLength; i++){
-			let toppingChecked = toppingName[i].checked;
 
-			if(toppingChecked){
-				toppingValue += toppingName[i].value;
-			}
-		}
-		let results = toppingValue
-		return results;
-			}
-		let pizzQuantity = parseInt($("#Quantity").val());
-		let mySize = price();
-		let myToppings = checkBox();
-		function checkBox(){
-			let check = document.getElementsByName("mole");
-			let box1 = 150
-			let box2 = 180
-			let box3 = 200
-			if(check[0].checked){
-				document.getElementById("errorMessage").innerHTML="";
-				return box1;
-			}else if(check[1].checked){
-				document.getElementById("errorMessage").innerHTML = "";
-				return box2;
-			}else if(check[2].checked){
-				document.getElementById("errorMessage").innerHTML = "";
-				return box3;
-			}
-		}
-		let checked = checkBox();
-		let shipping = addDelivery();
-		function addDelivery(){
-			let checkDelivery= document.getElementsByName("deliver");
-			if(checkDelivery[0].checked){
-				let money = 200;
-				return money;
-			}else if(!checkDelivery[1].checked){
-				let noMoney = 0;
-				return noMoney;
-				
-			}
-		}
-		let newTotal = new total(pizzaQuantity,pizzSize,pizzaCrust,pizzaToppings,pizzaDelivery);
-		if (name != " " && pizzaSize != " " && pizzaCrust != " " && pizzaQuantity != " "&& check ){
-			$(".hide").fadeIn
-		}
+		 
+			$('.show-locate').click(function(){
+					$('.deliveryButton').slideToggle();
+			});
 
-		
-	})
-})
+ 
+	
+	});
